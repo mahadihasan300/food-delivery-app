0475c50af2db51f7ea0e106cc92e917ca6d7bb90
Mahadi Hasan <md.mahadihasan300@gmail.com>
2023-09-03 15:48:57 +0600

    restaurant listing routing added

---
+++ src/app/restaurant-listing/components/restaurant-listing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurant-listing',
+  templateUrl: './restaurant-listing.component.html',
+  styleUrls: ['./restaurant-listing.component.css']
+})
+export class RestaurantListingComponent {
+
+}
